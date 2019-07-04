87323360260244f0d3af8f31c42f08755b7f2911
Everett Smith <evepsmith15@gmail.com>
2019-07-03 22:30:54 -0700

    fixed the sql

--- bamazon.sql
+++ bamazon.sql
@@ -16,48 +16,32 @@ CREATE TABLE products (
     PRIMARY KEY (id)
 );
 
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Seven Wishes", "book", "15.99", "4"), 
+("Super Bash Sisters", "video game", "59.99", "20"),
+("Elf Ocarina", "toys", "24.99", "0"),
+("Golden Butterflies", "album", "40.00", "1"),
+("Masamune", "toys", "169.00", "3"),
+("Phantom Mac", "electronics", "550.99", "30"),
+("Prince of the Skies", "book", "25.00", "66"),
+("Slay Station Portable", "electronics", "199.00", "23"),
+("Hero Tunic", "clothes", "212.00", "5"),
+("Wave Combat Book", "book", "40.00", "44"),
+("Hero Wig", "cosmetics", "55.00", "2");
+
 CREATE TABLE department (
 department_id INT NOT NULL AUTO_INCREMENT,
-department_name VARCHAR(30) NULL,
-over_head_costs INT NULL AUTO_INCREMENT,
-product_sales INT NULL AUTO_INCREMENT,
-total_profit INT NULL, 
+department_name VARCHAR(30) NOT NULL,
+over_head_costs DECIMAL (10, 2) NOT NULL,
+total_profit DECIMAL (10, 2) NOT NULL, 
 PRIMARY KEY(department_id)
 );
 
-SELECT * FROM products;
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("43", "Seven Wishes", "book", "15.99", "4");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("662", "Super Bash Sisters", "video game", "59.99", "20");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("851", "Elf Ocarina", "toys", "24.99", "0");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("69", "Golden Butterflies", "album", "40.00", "1");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("777", "Masamune", "toys", "169.00", "3");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("1202", "Phantom Mac", "electronics", "550.99", "30");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("50", "Prince of the Skies", "book", "25.00", "66");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("1300", "Slay Station Portable", "electronics", "199.00", "23");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("453", "Hero Tunic", "clothes", "212.00", "5");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("696", "Wave Combat Book", "book", "40.00", "44");
-
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ("454", "Hero Wig", "cosmetics", "55.00", "2");
+INSERT INTO department (department_name, over_head_costs, total_profit)
+VALUES 
+("TOYS", 3000.00, 1200.00);
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
